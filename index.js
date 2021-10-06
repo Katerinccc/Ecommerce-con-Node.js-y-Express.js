1692e63cdb2cf48eea1b1916bec2040c1c65ae89
Katerin713 <kccc0713@gmail.com>
2021-10-05 20:38:10 -0500

    Se crea servidor con express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("My App");
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+  res.send("Hola mi server en Express");
+});
+
+app.listen(port,()=>{
+  console.log(`Listening at http://localhost:${port}`)
+})
+
+
+
+
