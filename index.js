9d9538665e09411c6077684752bed576ef62b5d4
Katerin713 <kccc0713@gmail.com>
2021-10-06 22:01:49 -0500

    se crean endpoints para users, products y categories

--- index.js
+++ index.js
@@ -12,16 +12,70 @@ app.get("/home", (req, res) =>{
 });
 
 
-app.get("/categories", (req, res) =>{
-  res.send("Categorias");
+app.get("/categories/:categoryId/products/:productId", (req, res) =>{
+  const {categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
 });
 
 app.get("/products", (req, res) =>{
-  res.json({
-    name: "Product1",
-    price: 1000
-  });
+  res.json([
+    {
+      name: "tablet",
+      price: 1000,
+    },
+    {
+      name: "laptop",
+      price: 2500,
+    },
+    {
+      name: "mouse",
+      price: 70,
+    },
+  ]);
 });
+
+app.get("/categories", (req, res) =>{
+  res.json([
+    {
+      name: "Tech",
+    },
+    {
+      name: "Groceries",
+    },
+    {
+      name: "clothes",
+    },
+  ]);
+});
+
+app.get("/users", (req, res) =>{
+  res.json([
+    {
+      name: "Katerin",
+      last_name: "Vega",
+    },
+    {
+      name: "Miguel",
+      last_name: "Velandia",
+    },
+    {
+      name: "Katalina",
+      last_name: "Gomez",
+    },
+  ]);
+});
+
+app.get("/products/:id", (req, res) =>{
+  const {id} = req.params;
+  res.json({
+      id,
+      name: "Product1",
+      price: 1000,
+  });
+})
 
 
 app.listen(port,()=>{
