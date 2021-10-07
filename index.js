c9fd376105543b222b2ef748783a6dc1fc3b5748
Katerin713 <kccc0713@gmail.com>
2021-10-06 20:39:29 -0500

    se crean rutas en el servidor de Home, categories y products

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ app.get("/", (req, res) =>{
   res.send("Hola mi server en Express");
 });
 
-app.get("/products", (req, res) =>{
-  res.send("Productos");
+app.get("/home", (req, res) =>{
+  res.send("Home");
 });
+
 
 app.get("/categories", (req, res) =>{
   res.send("Categorias");
+});
+
+app.get("/products", (req, res) =>{
+  res.json({
+    name: "Product1",
+    price: 1000
+  });
 });
 
 
