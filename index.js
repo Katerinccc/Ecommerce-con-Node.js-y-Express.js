f9d7b54baa818944f10a58dd5eea74fd17332e03
Katerin713 <kccc0713@gmail.com>
2021-10-07 22:41:35 -0500

    se crea directorio de routes y se separan los por responsabilidades los procesos para llamarlos desde el index principal y exportando los modulos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const faker = require("faker");
+const routerApi = require("./routes")
 
 const app = express();
 const port = 3000;
@@ -13,71 +13,7 @@ app.get("/home", (req, res) =>{
   res.send("Home");
 });
 
-
-app.get("/categories/:categoryId/products/:productId", (req, res) =>{
-  const {categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId
-  });
-});
-
-//Los endpoints especificos deben declararsen antes de los endpoints dinamicos.
-
-//endpoints especificos
-app.get("/products/filter", (req, res) =>{
-  res.send("soy un filtro")
-});
-
-//endpoint dinamico
-app.get("/products", (req, res) =>{
-  const products = [];
-  const {size} = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.imageUrl(),
-    });
-  };
-  res.json(products);
-});
-
-app.get("/products/:id", (req, res) =>{
-  const {id} = req.params;
-  res.json({
-      id,
-      name: "Product1",
-      price: 1000,
-  });
-})
-
-app.get("/categories", (req, res) =>{
-  res.json([
-    {
-      name: "Tech",
-    },
-    {
-      name: "Groceries",
-    },
-    {
-      name: "clothes",
-    },
-  ]);
-});
-
-app.get("/users", (req, res) => {
-  const {limit, offset} = req.query;
-  if (limit && offset){
-    res.json({
-      limit,
-      offset
-    })
-  }else{
-    res.send("no hay parametros");
-  }
-})
+routerApi(app);
 
 app.listen(port,()=>{
   console.log(`Listening at http://localhost:${port}`)
