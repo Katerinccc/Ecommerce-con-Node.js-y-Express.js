7b64b9fde7998af2d41de60653456a5561bbc82b
Katerin713 <kccc0713@gmail.com>
2021-10-07 22:06:12 -0500

    se agrega la dependencia de faker para generar contenido para el proyecto y se crean endpoints utilizando queries para los productos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const express = require("express");
+const faker = require("faker");
+
 const app = express();
 const port = 3000;
 
@@ -20,22 +22,36 @@ app.get("/categories/:categoryId/products/:productId", (req, res) =>{
   });
 });
 
-app.get("/products", (req, res) =>{
-  res.json([
-    {
-      name: "tablet",
-      price: 1000,
-    },
-    {
-      name: "laptop",
-      price: 2500,
-    },
-    {
-      name: "mouse",
-      price: 70,
-    },
-  ]);
+//Los endpoints especificos deben declararsen antes de los endpoints dinamicos.
+
+//endpoints especificos
+app.get("/products/filter", (req, res) =>{
+  res.send("soy un filtro")
 });
+
+//endpoint dinamico
+app.get("/products", (req, res) =>{
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.imageUrl(),
+    });
+  };
+  res.json(products);
+});
+
+app.get("/products/:id", (req, res) =>{
+  const {id} = req.params;
+  res.json({
+      id,
+      name: "Product1",
+      price: 1000,
+  });
+})
 
 app.get("/categories", (req, res) =>{
   res.json([
@@ -51,32 +67,17 @@ app.get("/categories", (req, res) =>{
   ]);
 });
 
-app.get("/users", (req, res) =>{
-  res.json([
-    {
-      name: "Katerin",
-      last_name: "Vega",
-    },
-    {
-      name: "Miguel",
-      last_name: "Velandia",
-    },
-    {
-      name: "Katalina",
-      last_name: "Gomez",
-    },
-  ]);
-});
-
-app.get("/products/:id", (req, res) =>{
-  const {id} = req.params;
-  res.json({
-      id,
-      name: "Product1",
-      price: 1000,
-  });
+app.get("/users", (req, res) => {
+  const {limit, offset} = req.query;
+  if (limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  }else{
+    res.send("no hay parametros");
+  }
 })
-
 
 app.listen(port,()=>{
   console.log(`Listening at http://localhost:${port}`)
