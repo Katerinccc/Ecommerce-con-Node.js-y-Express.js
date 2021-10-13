92ecef8414a84267fb482d2434d01aa55fe3faac
Katerin713 <kccc0713@gmail.com>
2021-10-12 20:08:20 -0500

    Se crea la ruta para categorias y un versionador, adicional se agrega en productos el metodo post para ejecutar la API con la herramienta Insomnia

--- routes/products.router.js
+++ routes/products.router.js
@@ -32,6 +32,15 @@ router.get("/:id", (req, res) =>{
       name: "Product1",
       price: 1000,
   });
-})
+});
+
+router.post("/", (req, res) =>{
+  const body = req.body;
+  res.json({
+    message: "created",
+    data: body,
+  });
+});
+
 
 module.exports = router;
