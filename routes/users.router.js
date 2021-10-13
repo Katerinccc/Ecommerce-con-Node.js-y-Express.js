2a0493498b67cbde0409e5bec2173e28ceafbd22
Katerin713 <kccc0713@gmail.com>
2021-10-12 21:15:53 -0500

    Se crea una carpeta para separar los servicios y se crea en users metodo de post y delete

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,17 +1,47 @@
 const express = require("express");
-
+const faker = require("faker");
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-  const {limit, offset} = req.query;
-  if (limit && offset){
-    res.json({
-      limit,
-      offset
-    })
-  }else{
-    res.send("no hay parametros");
-  }
-})
+// router.get("/users", (req, res) => {
+//   const {limit, offset} = req.query;
+//   if (limit && offset){
+//     res.json({
+//       limit,
+//       offset
+//     })
+//   }else{
+//     res.send("no hay parametros");
+//   }
+// });
+
+router.get("/", (req, res) =>{
+  const users = [];
+  const {size} = req.query;
+  const limit = size || 5;
+  for (let index = 0; index < limit; index++){
+    users.push({
+      name: faker.name.firstName(),
+      lastname: faker.name.lastName(),
+      email: faker.internet.email(),
+    });
+  };
+  res.json(users);
+});
+
+router.post("/", (req, res) =>{
+  const body = req.body;
+  res.status(201).json({
+    message: "created",
+    data: body,
+  });
+});
+
+router.delete("/:id", (req, res) =>{
+  const {id} = req.params;
+  res.json({
+    message: "deleted",
+    id,
+  });
+});
 
 module.exports = router;
