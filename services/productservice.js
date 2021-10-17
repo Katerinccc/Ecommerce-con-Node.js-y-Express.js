386093ce63d3fc4e1497680b07ef6be52a4483f7
Katerin713 <kccc0713@gmail.com>
2021-10-16 19:46:48 -0500

    Se agrega nueva dependencia (JOI) para ayudarnos con la validación de datos

--- services/productservice.js
+++ services/productservice.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require("@hapi/boom");
 
 class ProductsService {
   constructor(){
@@ -13,6 +14,7 @@ class ProductsService {
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl(),
+        isBlock : faker.datatype.boolean()
       });
     };
   };
@@ -32,14 +34,26 @@ class ProductsService {
     });
   };
   async findOne(id) {
-    return this.products.find(item => item.id === id);
+    //Crear un error para probar errorHandler
+    // const name = this.getTotal();
+    const product = this.products.find(item => item.id === id);
+    if (!product){
+      throw boom.notFound("Product not found");
+    }
+    if (product.isBlock){
+      throw boom.conflict("Product is Block");
+    }
+    return product;
   };
   async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1){
-      throw new Error("product not found");
+      throw boom.notFound("Product not found");
     }
     const product = this.products[index];
+    if (product.isBlock){
+      throw boom.conflict("Product is Block");
+    }
     this.products[index] = {
       ...product,
       ...changes
@@ -49,7 +63,7 @@ class ProductsService {
   async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1){
-      throw new Error("product not found");
+      throw boom.notFound("Product not found");
     }
     this.products.splice(index, 1);
     return {id};
